7cf49405c7af472e2241e9e83c92b38a654bbc93
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-17 23:46:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function returnFirstTwoDrivers(){
+const driversreturnFirstTwoDrivers = function returnFirstTwoDrivers(){
   return drivers[0,1]
 }
